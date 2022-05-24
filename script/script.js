712b619e8c55e4271abfc35e6f38e768532075c3
buffaloluke <h.lukas.pacher@gmail.com>
2022-05-24 23:15:10 +0200

    Anpassungen Music-Seite

--- script/script.js
+++ script/script.js
@@ -5,12 +5,21 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
+  //Hole navbar li Elemente
+  let elems = document.querySelectorAll("li");
+  let length = elems.length;
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     //document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("band-title").style.fontSize = "5vh";
     document.getElementById("band-title").style.marginBottom = "0px";
+    for (let index = 0; index < length; index++) {
+      elems[index].style.fontSize = "4vh";
+    }
   } else {
     document.getElementById("band-title").style.fontSize = "8vh";
     document.getElementById("band-title").style.marginBottom = "0.67em";
+    for (let index = 0; index < length; index++) {
+      elems[index].style.fontSize = "5vh";
+    }
   }
 }
